a40de8183d456646bc4b99e26d74b971dd573e71
vadimbelinovich <vadimbelinovich@gmail.com>
2019-08-09 17:59:27 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import './js/main'
+// import './scss/main.scss'
+window.Vue = require('vue')
+import store from './store'
+Vue.component('example-component', require('./components/Example.vue').default)
+
+const app = new Vue({
+  data () {
+    return {
+      component: false,
+    }
+  },
+  store,
+  el: '#app'
+})
